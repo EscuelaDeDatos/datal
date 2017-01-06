40541418fe7c1b3265ab863f3b629a19aa8ddffb
Matias Saguir <mativs@gmail.com>
2017-01-06 10:30:58 -0300

    Try gevent monkey patching con patch_all

--- admin/wsgi.py
+++ admin/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
-from gevent import monkey; monkey.patch_socket()
+from gevent import monkey; monkey.patch_all()
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin.settings")
